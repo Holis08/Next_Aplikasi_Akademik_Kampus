15be4005c7cf81dae0e68b924a7b37e251e535d5
Holis08 <holisholis311@gmail.com>
2025-07-21 23:46:27 +0700

    first commit

---
+++ lib/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// Ganti dengan URL backend Laravel kamu
+const BASE_URL = "http://127.0.0.1:8000/api";
+
+const getToken = () => localStorage.getItem("token"); // atau dari cookies
+
+export const login = async (NIM: string, password: string) => {
+  const response = await axios.post(`${BASE_URL}/login`, {
+    NIM,
+    password,
+  });
+  return response.data;
+};
+
+export const logout = async (token: string | null) => {
+  if (!token) throw new Error("Token tidak ditemukan");
+
+  const res = await axios.post(
+    `${BASE_URL}/logout`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return res.data;
+};
